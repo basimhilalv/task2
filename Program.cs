86d713aad96a46e5b7b20d3d043e9ceb826f2b88
Basim Hilal <basimhilalofficial@gmail.com>
2025-04-25 15:42:05 +0530

    diamond pattern configured

--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
 ﻿Console.WriteLine("Enter the size : ");
 string? n = Console.ReadLine();
 int num = int.Parse(n);
+string s = "";
+for(int i=0;i<num;i++)
+{
+    for(int k=0;k<num-i;k++)
+    {
+        s += " ";
+    }
+
+    for(int j=0;j<=2*i;j++)
+    {
+        s += "*";
+    }
+    s += "\n";
+}
+for(int i=num;i>=0;i--)
+{
+    for(int k=num-i;k>0;k--)
+    {
+        s += " ";
+    }
+
+    for(int j=0;j<=2*i;j++)
+    {
+        s += "*";
+    }
+    s += "\n";
+}
+Console.WriteLine(s);
--- obj/Debug/net9.0/task2.AssemblyInfo.cs
+++ obj/Debug/net9.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eef068cabedff4c612ef57b2f76562ee6aea2c0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b6f8cbc73f1b9ef24d1672747609d81fc20df3")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
